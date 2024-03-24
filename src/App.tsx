004c358000f6f757887bdd8924ebbe668e09d9f3
Anton Boldyrev <boldyrev.webdev@gmail.com>
2024-03-24 15:45:35 +0500

    main: add initial files

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./styles/main.css"
+import "./styles/normalize.css"
+import "./styles/fonts.css"
+import "./styles/variables.css"
+
+function App() {
+  return <>hi</>
+}
+
+export default App
